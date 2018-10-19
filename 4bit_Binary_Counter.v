7528f0f41a1f61ca9aa4bb531fe9e59675ac875e
Rekkursion <35969022+Rekkursion@users.noreply.github.com>
2018-10-19 12:07:21 +0800

    Add files via upload

---
+++ 4bit_Binary_Counter.v
@@ -0,0 +1,115 @@
+`timescale 1ns/100ps
+
+module Binary_Counter_4bits(A_count, C_out, Data_in, CLK, Clear, Count, Load);
+	output reg [3:0]	A_count;
+	output reg			C_out;
+	input [3:0]			Data_in;
+	input				CLK;
+	input				Clear;
+	input				Count, Load;
+	
+	always @(posedge CLK or negedge Clear) begin
+		if(!Clear) begin
+			A_count <= 4'b0000;
+		end
+		else begin
+			if(Load == 1) begin
+				A_count <= Data_in;
+			end
+			else begin
+				if(Count == 1) begin
+					{C_out, A_count} <= A_count + 4'b0001;
+				end
+			end
+		end
+	end
+endmodule
+
+module t_Binary_Counter_4bits;
+	wire [3:0]			A_count;
+	wire				C_out;
+	reg [3:0]			Data_in;
+	reg 				CLK, Clear;
+	reg					Count, Load;
+	
+	Binary_Counter_4bits M(A_count, C_out, Data_in, CLK, Clear, Count, Load);
+	
+	initial begin
+		Data_in = 4'b0110;
+		CLK = 0;
+		Count = 1;
+		Load = 1;
+		
+		#30; Clear = 1;
+		#70; CLK = 1;
+		
+		#100; CLK = 0; Clear = 0;
+		
+		#30; Load = 0;
+		#20; Clear = 1;
+		#50; CLK = 1;
+		
+		#50; Count = 0;
+		#50; CLK = 0;
+		
+		#100; CLK = 1;
+		
+		#40; Count = 1;
+		#60; CLK = 0;
+		
+		#55; Load = 1;
+		#45; CLK = 1;
+		
+		#51; Data_in = 4'b1010;
+		#49; CLK = 0;
+		
+		#100; CLK = 1;
+		
+		#38; Load = 0;
+		#62; CLK = 0;
+		
+		#100; CLK = 1;
+		
+		#100; CLK = 0;
+		
+		#100; CLK = 1;
+		
+		#100; CLK = 0;
+		
+		#100; CLK = 1;
+		
+		#100; CLK = 0;
+		
+		#100; CLK = 1;
+		
+		#100; CLK = 0;
+		
+		#100; CLK = 1;
+		
+		#100; CLK = 0;
+		
+		#100; CLK = 1;
+		
+		#100; CLK = 0;
+		
+		#100; CLK = 1;
+		
+		#100; CLK = 0;
+		
+		#100; CLK = 1;
+		
+		#100; CLK = 0;
+		
+		#100; CLK = 1;
+		
+		#100; CLK = 0;
+		
+		#100; CLK = 1;
+		
+		
+		
+	end
+	initial #3000 $finish;
+	initial $dumpvars;
+	
+endmodule
---
+++ shift_reg.v
@@ -0,0 +1,92 @@
+`timescale 1ns/100ps
+
+module Shift_Register(A_par, I_par, CLK, LSB_in, MSB_in, Sel, Clear);
+	output reg [3:0]	A_par;
+	input [3:0]			I_par;
+	input				CLK;
+	input				Clear;
+	input				LSB_in, MSB_in;
+	input [1:0]			Sel;
+	
+	always @(posedge CLK or negedge Clear) begin
+		if(!Clear) begin
+			A_par <= 4'b0000;
+		end
+		else begin
+			case (Sel)
+				2'b00: ;
+				2'b01: A_par <= {MSB_in, A_par[3:1]};
+				2'b10: A_par <= {A_par[2:0], LSB_in};
+				2'b11: A_par <= I_par;
+				default: ;
+			endcase
+		end
+	end
+endmodule
+
+module t_Shift_Register;
+	wire [3:0]			A_par;
+	reg [3:0]			I_par;
+	reg 				CLK, LSB_in, MSB_in, Clear;
+	reg [1:0]			Sel;
+	
+	Shift_Register M(A_par, I_par, CLK, LSB_in, MSB_in, Sel, Clear);
+	
+	initial begin
+		I_par = 4'b0110;
+		LSB_in = 1;
+		MSB_in = 0;
+		CLK = 0;
+		Sel = 2'b11;
+		
+		#30; Clear = 1;
+		#70; CLK = 1;
+		
+		#100; CLK = 0; Clear = 0;
+		
+		#30; Sel = 2'b10;
+		#20; Clear = 1;
+		#50; CLK = 1;
+		
+		#50; LSB_in = 0;
+		#50; CLK = 0;
+		
+		#100; CLK = 1;
+		
+		#40; Sel = 2'b01;
+		#60; CLK = 0;
+		
+		#100; CLK = 1;
+		
+		#39; MSB_in = ~MSB_in;
+		#61; CLK = 0;
+		
+		#100; CLK = 1;
+		
+		#45; MSB_in = ~MSB_in;
+		#55; CLK = 0;
+		
+		#40; Sel = 2'b11;
+		#30; I_par = 4'b1101;
+		#30; CLK = 1;
+		
+		#70; Sel = 2'b01;
+		#30; CLK = 0;
+		
+		#100; CLK = 1;
+		
+		#34; Sel = 2'b00;
+		#66; CLK = 0;
+		
+		#100; CLK = 1;
+		
+		#47; Sel = 2'b11;
+		#53; CLK = 0;
+		
+		#100; CLK = 1;
+		
+	end
+	initial #3000 $finish;
+	initial $dumpvars;
+	
+endmodule
